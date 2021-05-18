22ac5149aa5224e486db8587305b6de0ba193320
chenxian <1761219353@qq.com>
2021-05-18 09:55:16 +0800

    我修改了内容！

--- src/com/lisa/git/TestGit01.java
+++ src/com/lisa/git/TestGit01.java
@@ -3,6 +3,7 @@ package com.lisa.git;
 public class TestGit01 {
     public static void main(String[] args) {
         System.out.println("git测试01");
+        System.out.println("我修改了内容！");
     }
 
 }
